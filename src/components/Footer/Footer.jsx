ce7e23cad155efff26da89662d36c4b047551e86
Miwanja <misha787981@gmail.com>
2024-05-08 00:23:45 +0300

    laid out and stylized Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import './Footer.css'
+import { assets } from '../../assets/assets'
 
 function Footer() {
   return (
-    <div className='footer'>
-      
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga minima perferendis dolorem commodi quod voluptatum ullam natus, omnis earum dolorum pariatur excepturi modi labore error iure, odio tenetur voluptate provident repellat, impedit eos debitis sit a explicabo? Consectetur corporis dolorum facilis saepe et alias suscipit ut culpa illum reiciendis incidunt adipisci, perspiciatis doloremque nulla tempora numquam iste inventore pariatur.</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+8-931-222-43-43</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Tomato.com - All Right Reserved.
+      </p>
     </div>
   )
 }
