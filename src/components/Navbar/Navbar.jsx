0de9d956d323c11271f3bfbb16d103dccea4d117
Miwanja <misha787981@gmail.com>
2024-05-08 21:15:44 +0300

    made media queries, added html link instead of li

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
 
@@ -9,11 +10,11 @@ export default function Navbar() {
   return (
     <div className='navbar'>
        <img src={assets.logo} alt="" className="logo" />
-       <ul className="navbar-manu">
-        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
-        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
-        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+       <ul className="navbar-menu">
+        <Link onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+        <Link onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</Link>
+        <Link onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</Link>
+        <Link onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</Link>
        </ul>
        <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
