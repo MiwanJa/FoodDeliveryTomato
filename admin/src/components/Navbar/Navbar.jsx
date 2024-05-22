d09ea5d049a2bcc3d617980ab1467897e2b2cc75
Miwanja <misha787981@gmail.com>
2024-05-22 15:09:55 +0300

    create style for Navbar

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <img
+       className='logo'
+       src={assets.logo} 
+       alt=""
+       loading="lazy"
+       />
+      <img
+       className='profile'
+       src={assets.profile_image} 
+       alt=""
+       loading="lazy" 
+       />
     </div>
   )
 }
