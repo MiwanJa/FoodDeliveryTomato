5191695ecd7f2124f274d205ecee8087f764ae8a
Miwanja <misha787981@gmail.com>
2024-05-25 12:52:28 +0300

    added a new item to the food table, added the react-toastify library

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import './Orders.css'
-import { assets } from '../../assets/assets'
 
 const Orders = () => {
   return (
     <div className='add'>
-      <form className="flex-col">
-        <div className="add-img-upload flex-col">
-          <p>Upload Image</p>
-          <label htmlFor="image">
-            <img src={assets.upload_area} alt="" />
-          </label>
-          <input type="file" id='image' hidden/>
-        </div>
-      </form>
+     
     </div>
   )
 }
