5912802d26397cf1a4d8938ceaba444e53163d32
Miwanja <misha787981@gmail.com>
2024-05-29 17:44:23 +0300

    loginPopup + StoreContext upDate

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -4,7 +4,10 @@ import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+
   const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:4000";
+  const [token, setToken] = useState("");
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -36,6 +39,9 @@ const StoreContextProvider = (props) => {
     addToCart,
     removeFromCart,
     getTotalCartAmount,
+    url,
+    token,
+    setToken
   };
 
   return (
