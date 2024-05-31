2eeb431b080b094cd998e848d141d3695a2e03e3
Miwanja <misha787981@gmail.com>
2024-05-31 19:56:00 +0300

    upDate auth.js 'try-catch block'

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -2,6 +2,20 @@ import jwt from "jsonwebtoken"
 
 
 const authMiddleware = async (req,res,next)=>{
+    const [token] = res.headers;
+    if (!token) {
+        return res.json({success:false,message:"Not Authorized Login Again"})
+    }
+
+    try {
+        const token_decode = jwt.verify(token,process.env.JWT_SECRET);
+        req.body.userId = token_decode.id;
+        next();
+    } catch (error) {
+        console.log("Error");
+        res.json({success:false,message:"Error"})
+    }
+
 
 }
 
